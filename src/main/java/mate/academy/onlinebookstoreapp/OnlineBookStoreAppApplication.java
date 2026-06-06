337f8944de8830c700c762d5a7078437c2c3558f
xAlexandrosx <olek4004@gmail.com>
2026-06-06 14:13:07 +0200

    Initial commit

---
+++ src/main/java/mate/academy/onlinebookstoreapp/OnlineBookStoreAppApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.onlinebookstoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreAppApplication.class, args);
+	}
+
+}
